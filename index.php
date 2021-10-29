82b580aab327dea7820b4844fb964548db203c84
Cha0sType <chaostypespam@gmail.com>
2021-10-29 21:10:41 +0200

    Started so pretty basic formatting of the project

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum</title>
+    <link rel="stylesheet" href="stylesheets/index.css">
 </head>
 <body>
-    <a href="./login.php">Login</button>
+        <a href="login/login.php">
+            <button>Login</button>
+        </a>
 </body>
 </html>
