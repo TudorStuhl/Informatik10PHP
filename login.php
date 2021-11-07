d6b62654098c8aaa02d49554df3bc3fe5ed85ef3
maron211 <marc.bamberger@freenet.de>
2021-11-07 01:10:47 +0100

    fixed the login page

--- login.php
+++ login.php
@@ -5,32 +5,33 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="?login=1" method="post">
-        <input type="email" placeholder="E-Mail"><br>
-        <input type="password" name="password" placeholder="Passwort"><br>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <input name="email" placeholder="E-Mail"><br>
+        <input name="password" type="password" placeholder="Passwort"><br>
         <input type="submit" value="Anmelden">
     </form>
 
     <?php
     session_start();
     $pdo = new PDO('mysql:host=localhost;dbname=forum', 'root', '');
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
-    if (isset($_GET['login'])) {
+        $password = $_POST['password'];
         $email = $_POST['email'];
-        $passwort = $_POST['password'];
+        
 
         $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
         $result = $statement->execute(array('email' => $email));
         $user = $statement->fetch();
 
-        if ($user !== false && password_verify($passwort, $user['passwort'])) {
-            $_SESSION['userid'] = $user['id'];
+        if ($user !== false && password_verify($password, $user['pwd_hash'])) {
+            $_SESSION['userid'] = $user['ID'];
             die("Login erfolgreich, weiter zum Forum: <br> <a href= test.php><button>Forum</button></a>");
         } else {
             die("Email oder Passwort sind falsch!");
         }
     }
-
+    
     ?>
 </body>
 </html>
