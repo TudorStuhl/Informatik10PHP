b251bb9e4035429bab4d73eb74d74c83b0a5da6a
Tudor Stuhl <69587845+TudorStuhl@users.noreply.github.com>
2022-03-28 21:00:28 +0200

    Added images, started creating the template for the finished website

---
+++ index2.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="./stylesheets/index2.css">
+</head>
+<body>
+    <div id="navbar">
+        <span class="material-icons">home</span>
+        <span class="material-icons">edit_note</span>
+        <span class="material-icons">logout</span>
+    </div>
+    <div id="sidebar">
+        <img id="avatar" src="./img/avatar.svg" alt="avatar">
+        <h2>Otto von Bismarck</h2>
+        <h3>otto.von.bismarck@gmail.com</h3>
+        <h4>Accountmenü</h4>
+        <span>
+            <a href="">Nutzernamen ändern</a><br>
+            <a href="">Email ändern</a><br>
+            <a href="">Passwort ändern</a>
+        </span>
+        <br><br><br><br><br>
+        <span id="logout-side" class="material-icons">logout</span>
+    </div>
+    <content>
+        <img id="reload" src="./img/reload.svg" alt="reload">
+        <h1>Vorgeschlagene Fragen</h1>
+        <div class="question g">
+            <div>
+                <span class="name">bwl_anton32</span><br>
+                <span class="answers">10<br>Antworten</span>
+            </div>
+            <h2>Warum ist das Wort "Abkürzung" länger als "Umweg"??</h2>
+            <p>
+                Hallo Leutis, also ich hab jz mal so ne Fraaage… Warum ist das Wort "Abkürzung"
+                eig länger als "Umweg"??? Macht das überhaupt Sinn!!1!11!!!elf!!!!
+                Wäre echt schnieke, wenn das mal einer beantworten könnte.
+            </p>
+        </div>
+        <div class="question b">
+            <div>
+                <span class="name">bwl_anton32</span><br>
+                <span class="answers">10<br>Antworten</span>
+            </div>
+            <h2>Warum ist das Wort "Abkürzung" länger als "Umweg"??</h2>
+            <p>
+                Hallo Leutis, also ich hab jz mal so ne Fraaage… Warum ist das Wort "Abkürzung"
+                eig länger als "Umweg"??? Macht das überhaupt Sinn!!1!11!!!elf!!!!
+                Wäre echt schnieke, wenn das mal einer beantworten könnte.
+            </p>
+        </div>
+        <div class="question r">
+            <div>
+                <span class="name">bwl_anton32</span><br>
+                <span class="answers">10<br>Antworten</span>
+            </div>
+            <h2>Warum ist das Wort "Abkürzung" länger als "Umweg"??</h2>
+            <p>
+                Hallo Leutis, also ich hab jz mal so ne Fraaage… Warum ist das Wort "Abkürzung"
+                eig länger als "Umweg"??? Macht das überhaupt Sinn!!1!11!!!elf!!!!
+                Wäre echt schnieke, wenn das mal einer beantworten könnte.
+            </p>
+        </div>
+    </content>
+</body>
+</html>
