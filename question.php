20d9f70765e7e354a425b96b77278079979306f1
Cha0sType <chaostypespam@gmail.com>
2022-04-06 22:09:29 +0200

    80% finished the display for question and answers

--- question.php
+++ question.php
@@ -44,33 +44,87 @@ session_start();
     </div>
     </div>
     <content>
-    <questions>
-    <div class="question r">
-        <div class="topic">
-            <span class="name", text-align="center">Warum ist die Banane krumm und nicht dreieckig?</span>
-        </div>
-        <div class="user">
+    <?php
+        if (!isset($_GET["id"])) {
+            echo "Fehler: keine Frage abgerufen.";
+        }
+        else {
 
-        </div>
-        <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat vero ipsam aliquam accusamus? Veritatis, hic reprehenderit corporis eius, placeat sed officiis possimus delectus voluptatem repudiandae sint nam, assumenda soluta quaerat.
-        </p>
-        
-        <?php
+            //Connecting to the database and fetching the needed information
             $file = file_get_contents('database_config.json');
             $data = json_decode($file, True);
             $con = new mysqli($data["host"], $data["user"], $data["password"], $data["database"]);
             $question_id = $_GET["id"];
             $res = $con -> query("SELECT * FROM entries WHERE ID = $question_id");
             $i = $res -> fetch_assoc();
-            
+            $topic = $i["topic"];
+            $content = $i["content"];
+            $user_id = $i["user_id"];
 
-        ?>
-    </div> 
-        </questions>   
-    <answers>
+            //Converting from Y/M/D H/I/S to D/M/Y date format
+            $new_datetime = DateTime::createFromFormat("Y-m-d H:i:s", $i["date"]);
+            $date = "Am ". $new_datetime -> format("d.m.y");
 
-    <answers>
+            //Querry and fetch the user from the database
+            $res = $con -> query("SELECT username FROM users WHERE ID = $user_id");
+            $b = $res -> fetch_array();
+            $username = $b[0];
+       
+            //Display the question with the content
+            echo "<div class='question'>
+                <div class='topic r'>
+                    <span class='name', text-align='center'>$topic</span>
+                </div>
+                <div class='user'>
+                    <img src='./img/avatar.svg' alt='avatar' style='margin-top: 80px;'>
+                    <h3>$username</h3>
+                    <span>$date</span>
+                </div>
+                <p>
+                   $content     
+                </p>
+                
+
+            </div>";
+            //Query all replies from the database
+            $res = $con -> query("SELECT * FROM replies WHERE entry_id = $question_id");
+            if ($res -> num_rows > 0) {  
+                while($i = $res->fetch_assoc()) {
+                    $user_id = $i["user_id"];
+
+                    //Converting from Y/M/D H/I/S to D/M/Y date format
+                    $new_datetime = DateTime::createFromFormat("Y-m-d H:i:s", $i["date"]);
+                    $date = "Am ". $new_datetime -> format("d.m.y");
+                    $content = $i["content"];
+
+                    //Query and fetch the user
+                    $res = $con -> query("SELECT username FROM users WHERE ID = $user_id");
+                    $b = $res -> fetch_array();
+                    $username = $b[0];
+                    
+                    //Display the answer with the content
+                    echo "
+                    <div class='answer'>
+                        <div class='user'>
+                            <img src='./img/avatar.svg' alt='avatar'>
+                            <h3>$username</h3>
+                            <span>$date</span>
+                        </div>
+                        <p>
+                            $content
+                        </p>
+                    </div>
+                    
+                    
+                    ";
+                }
+        }
+        else {
+            //If there are no replies in the database, give the user a feedback
+            echo "<p class='anti-answer'>Es gibt noch keine Antworten auf diese Frage.</p>";
+        }
+    }
+    ?>
     <content>
 </body>
 </html>
